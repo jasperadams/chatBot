7218a414e59f646fa092587788e5cba710d02f47
jasperadams <jasperadams@students.berkeley.net>
2018-05-31 10:05:05 -0700

    update

---
+++ q&a.js
@@ -0,0 +1,12 @@
+
+var q1 = {
+    question: '<h1>what is your name?</h1>',
+    answer: '<h1>hello ' + input + '</h1>',
+    time: 2400
+};
+
+var q1 = {
+    question: '<h1>what is your name?</h1>',
+    answer: '<h1>hello ' + input + '</h1>',
+    time: 2400
+};
